c9d9e414b1bf1bc03cdc056c70a5199127e00fa3
NguyenNgan1211 <128135160+NguyenNgan1211@users.noreply.github.com>
2023-06-24 21:36:19 +0700

    Update 2_1.cpp

--- tuan_2/2_1.cpp
+++ tuan_2/2_1.cpp
@@ -3,18 +3,18 @@
 std::string words(int number)
 {
 	if (number >= 1 && number <= 9) {
-	std::string numbers[] = {"one","two","three","four","five","six","seven","eight","nine","even","odd"};
-	return numbers[number-1];
+		std::string numbers[] = {"one","two","three","four","five","six","seven","eight","nine","even","odd"};
+		return numbers[number-1];
 	}
 	
 	else if (number > 9) {
-        if (number % 2 == 0) {
-            return "even";
-        } else {
-            return "odd";
-        }
-    }
-    return "";
+        	if (number % 2 == 0) {
+           	 return "even";
+       	}
+	else 
+            return "odd";  
+    	}
+    	return "";
 }
 int main(){
 	int a,b;
@@ -22,10 +22,10 @@ int main(){
 	if(a<=b ){
 		for(int i = a ; i<= b; i++){
 			std::cout<<words(i)<<std::endl;
+		}	
 	}
-}
 	else {
 		std::cout<<words(a)<<std::endl;
 		std::cout<<words(b)<<std::endl;
-}
+	}
 }
